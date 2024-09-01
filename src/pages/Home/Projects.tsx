45702d883d9ed0faee9be485ca592a8b6cf61381
Shrey Prajapati <shreyprajapati13@gmail.com>
2024-09-01 21:54:56 +0530

    ref: modify cards

--- src/pages/Home/Projects.tsx
+++ src/pages/Home/Projects.tsx
@@ -1,7 +1,9 @@
 import Button from "../../components/Button";
-import tileMasterMockUpImage from "../../assets/Tile-Master-MockUp.png";
+import tileMasterMockUpImage from "../../assets/Group 16.png";
+import quickBiteMockupImage from '../../assets/QuickbytesMockup.png'
 
 export default function Projects() {
+  
   return (
     <>
       <section className="animate bg-[var(--color-black-background) mt-8 w-full items-center justify-center p-4 md:mt-12 md:px-8 lg:mb-36 lg:flex">
@@ -16,7 +18,8 @@ export default function Projects() {
 
           <span className="horizontal_wraper item-center flex w-full justify-center">
             <span className="horizontal_container grid w-full grid-flow-col grid-rows-1 gap-4 overflow-x-scroll md:grid-cols-2 md:grid-rows-2 md:gap-8 lg:grid-cols-3 lg:grid-rows-1">
-              <Card />
+              <Card image={tileMasterMockUpImage} title='Tile Master Game'/>
+              <Card image={quickBiteMockupImage} title='QuickBite'/>
             </span>
           </span>
         </div>
@@ -25,16 +28,22 @@ export default function Projects() {
   );
 }
 
-function Card() {
+function Card({image, title, url}:{image:string, title:string, url?:string}) {
   return (
     <>
-      <div className="horizontal_card h-[24rem] w-[92vw] rounded-[12px] bg-[var(--color-black-foreground)] md:w-full lg:w-[300px]">
-        <span className='h-24 borer-white border-2 border-solid'>
-          <figure>
-            <img src={tileMasterMockUpImage} alt="Image" className="fit-cover h-[10rem] rounded-xl" />
-          </figure>
+      <div className="horizontal_card flex h-[24rem] w-[92vw] flex-col gap-4 rounded-[12px] bg-[var(--color-black-foreground)] p-4 md:w-full lg:w-[300px]">
+        <div className="fit- flex h-56 items-center justify-center overflow-hidden rounded-xl">
+          <img
+            src={image}
+            alt="Image"
+            className=" transition-transform duration-700 hover:scale-[1.2] hover:blur-[1px] "
+          />
+        </div>
+        <span className="flex">
+          <h1 className="flex font-Poppins text-xl font-bold text-white">
+            {title}{url}
+          </h1>
         </span>
-        Hello from div
       </div>
     </>
   );
